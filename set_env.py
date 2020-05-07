cb1eddd6647d279f17398fe8819b9207d71cab68
zhaoshengzhi <iloveyesterday@outlook.com>
2020-05-07 21:50:17 +0800

    format

--- set_env.py
+++ set_env.py
@@ -11,25 +11,27 @@ envs = [
     ('ZOOKEEPER_HOME', r'C:\Walter\development\zookeeper-3.4.14')
 ]
 
-GET_ENV=r'set {key}'
-SET_ENV=r'setx {key} "{value}"'
+GET_ENV = r'set {key}'
+SET_ENV = r'setx {key} "{value}"'
 
 
 def set_env(key, value):
-    get_cmds = GET_ENV.format(key=key)
-    adb_proc = subprocess.Popen(get_cmds, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+    get_cmd = GET_ENV.format(key=key)
+    adb_proc = subprocess.Popen(get_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                shell=True)
     (output, err) = adb_proc.communicate()
     env = output.decode('gbk')
     old_value = ''
-    if env != None and env != '':
+    if env is not None and env != '':
         m = re.match(r'^\w+?=(.*)', env)
         if m:
             old_value = m.group(1)
 
-    if old_value != None and old_value != '':
+    if old_value is not None and old_value != '':
         value = '{};{}'.format(value, old_value)
-    set_cmds = SET_ENV.format(key=key, value=value)
-    adb_proc = subprocess.Popen(set_cmds, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+    set_cmd = SET_ENV.format(key=key, value=value)
+    adb_proc = subprocess.Popen(set_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                shell=True)
     (output, err) = adb_proc.communicate()
     print(output.decode('gbk'))
     print(err.decode('gbk'))
