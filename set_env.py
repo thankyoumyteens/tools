cc64240f56a8aa3b594a62afebcc856f5a2e7262
zhaoshengzhi <iloveyesterday@outlook.com>
2020-05-10 09:38:30 +0800

    format

--- set_env.py
+++ set_env.py
@@ -8,6 +8,7 @@ import re
 envs = [
     ('JAVA_HOME', r'C:\Walter\development\jdk1.8.0_232'),
     ('MAVEN_HOME', r'C:\Walter\development\apache-maven-3.5.3'),
+    ('ERLANG_HOME', r'C:\Walter\development\erl5.10.4'),
     ('ZOOKEEPER_HOME', r'C:\Walter\development\zookeeper-3.4.14')
 ]
 
@@ -16,26 +17,30 @@ SET_ENV = r'setx {key} "{value}"'
 
 
 def set_env(key, value):
-    get_cmd = GET_ENV.format(key=key)
-    adb_proc = subprocess.Popen(get_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                shell=True)
-    (output, err) = adb_proc.communicate()
-    env = output.decode('gbk')
-    old_value = ''
-    if env is not None and env != '':
-        m = re.match(r'^\w+?=(.*)', env)
-        if m:
-            old_value = m.group(1)
-
-    if old_value is not None and old_value != '':
-        value = '{};{}'.format(value, old_value)
+    print('{} = {}'.format(key, value))
     set_cmd = SET_ENV.format(key=key, value=value)
-    adb_proc = subprocess.Popen(set_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+    adb_proc = subprocess.Popen(set_cmd,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
                                 shell=True)
     (output, err) = adb_proc.communicate()
     print(output.decode('gbk'))
     print(err.decode('gbk'))
 
 
+path_env = ''
 for item in envs:
+    path_env = r'{}%{}%\bin;'.format(path_env, item[0])
     set_env(item[0], item[1])
+
+old_path = r'C:\Users\ILove\anaconda3;' \
+           r'C:\Users\ILove\anaconda3\Library\mingw-w64\bin;' \
+           r'C:\Users\ILove\anaconda3\Library\usr\bin;' \
+           r'C:\Users\ILove\anaconda3\Library\bin;' \
+           r'C:\Users\ILove\anaconda3\Scripts;' \
+           r'%USERPROFILE%\AppData\Local\Microsoft\WindowsApps;' \
+           r'C:\Program Files\Bandizip\;' \
+           r'C:\Users\ILove\AppData\Roaming\npm;' \
+           r'C:\Users\ILove\AppData\Local\Programs\Microsoft VS Code\bin;'
+set_env('Path', '{}{}'.format(old_path, path_env))
